31e02c06936b2964aa9d668cf9d5ecb50bfabe8a
andres1m <andreusar11@gmail.com>
2025-01-16 18:41:56 +0500

    Init commit

---
+++ src/main/java/ru/andres1m/SLogs/SLogsApplication.java
@@ -0,0 +1,13 @@
+package ru.andres1m.SLogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andres1m/SLogs/SLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andres1m.SLogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
